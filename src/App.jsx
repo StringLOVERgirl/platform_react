c28a1dc617b637670b3dc1afb28a74ca19955cf8
StringLOVERgirl <marchenkostar@gmail.com>
2025-09-12 09:38:13 +0300

    mobile adapt

--- src/App.jsx
+++ src/App.jsx
@@ -133,8 +133,7 @@ window.innerWidth > 600 ? window.addEventListener('mousemove', unBlur) : window.
   
   function unBlur(e){
     // отключаем прокрутку при свайпе
-  event.preventDefault(); // отключаем прокрутку при свайпе
-  e.touches[0]
+  e.preventDefault(); // отключаем прокрутку при свайпе
   const evenet = window.innerWidth < 600 ? e.touches[0] : e
   // window.innerWidth < 600? e.preventDefault():""
     const x = evenet.clientX;
