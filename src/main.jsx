e56b903bc7324454033f7b142ad41d9ec94cca2f
StringLOVERgirl <marchenkostar@gmail.com>
2025-09-16 06:38:45 +0300

    adapt + video

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,8 @@ import { store } from '../js/routers'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 )
