3351fad0190b72ad1f92520bb60818d497fba3f7
StringLOVERgirl <marchenkostar@gmail.com>
2025-09-16 05:49:07 +0300

    adapt + video

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import "./App.css";
 import '../styles/more.css'
 import './index.css'
 import { Provider } from 'react-redux'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter,HashRouter } from 'react-router-dom'
 import { store } from '../js/routers'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
